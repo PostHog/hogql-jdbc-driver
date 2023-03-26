045f25c78044386f3f894b8db09d2fcf78b743ad
Marius Andra <marius.andra@gmail.com>
2023-03-26 18:25:33 -0400

    remove files we don't need

---
+++ src/main/java/com/posthog/hogql/UniqueIdUtil.java
@@ -0,0 +1,21 @@
+package com.posthog.hogql;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public final class UniqueIdUtil {
+
+    private UniqueIdUtil() {
+    }
+
+    //threadsafe
+    private static final SecureRandom secureRandom = new SecureRandom();
+    //threadsafe
+    private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder();
+
+    public static String generateQueryId() {
+        byte[] randomBytes = new byte[16];
+        secureRandom.nextBytes(randomBytes);
+        return base64Encoder.encodeToString(randomBytes);
+    }
+
+}
